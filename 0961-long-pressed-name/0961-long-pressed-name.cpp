ec35d58f42a919e8b29f61a348ee9132ad10a8ea
Md. Tanvir Ahmed <88228281+Alraaafi@users.noreply.github.com>
2025-09-06 21:03:15 +0600

    Time: 0 ms (100%), Space: 8.8 MB (45.2%) - LeetHub

---
+++ 0961-long-pressed-name/0961-long-pressed-name.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool isLongPressedName(string &a, string &t) {
+        int n = a.size();
+        int m = t.size();
+
+        int i,j;
+
+        bool f = true;
+
+        i = 0;
+        j = 0;
+
+        char ch,c2;
+        for( ;i<n && j<m ; )
+        {
+            if( a[i]==t[j])
+            {
+                ch = t[j];
+                c2 = a[i];
+                i++;
+                j++;
+                
+            }
+            else
+            {
+                ch = t[j];
+                j++;
+                if( c2!=ch )
+                    return false;
+                
+
+                //if( j>0 && t[j-1] j )
+            }
+        }
+
+        cout<<i<<" "<<j<<" "<<ch<<" "<<t[j]<<endl;
+        for( ;j<m; j++ )
+        {
+            if( t[j]!= ch ){
+                return false;
+            }
+            ch = t[j];
+        }
+
+        
+
+        if( i==n && j==m ) f = true;
+        else f = false;
+
+        return f;
+    }
+};
